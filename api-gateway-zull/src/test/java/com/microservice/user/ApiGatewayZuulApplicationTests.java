f5ff4ab7c34e7356270991a8caf6f550693a0b48
rayanabonfanti <rayanabonfanti@gmail.com>
2023-11-05 20:53:27 -0300

    fix: update project

--- api-gateway-zull/src/test/java/com/microservice/user/ApiGatewayZuulApplicationTests.java
+++ api-gateway-zull/src/test/java/com/microservice/user/ApiGatewayZuulApplicationTests.java
@@ -2,12 +2,15 @@ package com.microservice.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class ApiGatewayZuulApplicationTests {
 
 	@Test
 	void contextLoads() {
+    ApiGatewayZuulApplication.main(new String[]{});
+    assertTrue(true);
 	}
 
 }
---
+++ api-gateway-zull/src/test/java/com/microservice/user/config/TokenJwtConfigTest.java
@@ -0,0 +1,4 @@
+//package com.microservice.user.config;
+//
+//class TokenJwtConfigTest {
+//}
